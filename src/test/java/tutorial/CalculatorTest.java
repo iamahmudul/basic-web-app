420f6744d8fada7938aeac49fd0f8ad9167613a8
Mahmudul Islam <iamahmudul@gmail.com>
2017-11-19 16:43:05 +0600

    ....

---
+++ src/test/java/tutorial/CalculatorTest.java
@@ -0,0 +1,18 @@
+package tutorial;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mahmudul Islam on 11/19/2017.
+ */
+public class CalculatorTest {
+
+    @Test
+    public void testAbs(){
+
+        assertEquals(2,1);
+
+    }
+}
